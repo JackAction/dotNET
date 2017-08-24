93659aade7c717a78e8dcb08d11aada4228b3d88
Fabian <JackAction>
2017-08-24 21:20:15 +0200

    Beispiel 1

---
+++ Beispiel1/Beispiel1/Controllers/AutoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Beispiel1.Controllers
+{
+    public class AutoController : Controller
+    {
+        public string fahren()
+        {
+            return "faaahre";
+        }
+
+        public string schalteInGang(int gang)
+        {
+            return $"aktueller Gang: {gang}";
+        }
+
+        public ActionResult Index()
+        {
+            return Content("yuhuuu");
+        }
+
+        public ActionResult Message()
+        {
+            return Content("<script>alert('Welcome To All');</script>");
+        }
+    }
+}
---
+++ Beispiel1/Beispiel1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Beispiel1.MvcApplication" Language="C#" %>
